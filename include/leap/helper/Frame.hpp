572e0feeff9303528218744ecc891d2a9561f5f9
Malte Quandt <malte.quandt@student.fh-kiel.de>
2023-06-04 16:17:32 +0200

    feat: frame wrapper

---
+++ include/leap/helper/Frame.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <godot_cpp/classes/node.hpp>
+
+#include <Leap.h>
+
+#include <leap/helper/hands/Hand.hpp>
+#include <leap/helper/hands/HandList.hpp>
+#include <leap/helper/InteractionBox.hpp>
+
+#include <godot_cpp/variant/variant.hpp>
+#include <godot_cpp/classes/object.hpp>
+
+// for more informations, view https://developer-archive.leapmotion.com/documentation/v2/cpp/api/Leap.InteractionBox.html
+// this is effectively the same interface
+
+namespace godot::leap {
+class Frame : public Node {
+  GDCLASS(Frame, Node);
+public:
+  explicit Frame();
+  explicit Frame(Leap::Frame frame) : frame{frame} {}
+// Leap Methods:
+// -------------
+
+  float currentFramesPerSecond();
+  int64_t id();
+  bool isValid();
+
+  godot::leap::Hand* hand(int handID);
+  godot::leap::HandList* hands();
+  godot::leap::InteractionBox* interactionBox();
+
+protected:
+  static void _bind_methods();
+private:
+  Leap::Frame frame;
+
+  // pointers
+  std::shared_ptr<godot::leap::InteractionBox> box{nullptr};
+  std::shared_ptr<godot::leap::HandList> handList{nullptr};
+};
+
+} // namespace godot
---
+++ src/leap/helper/Frame.cpp
@@ -0,0 +1,50 @@
+#include <leap/helper/Frame.hpp>
+#include <leap/helper/InteractionBox.hpp>
+
+using namespace godot::leap;
+using namespace godot;
+
+Frame::Frame() {}
+
+float Frame::currentFramesPerSecond() {
+  return this->frame.currentFramesPerSecond();
+}
+
+int64_t Frame::id() {
+  return this->frame.id();
+}
+
+bool Frame::isValid() {
+  return this->frame.isValid();
+}
+
+Hand* Frame::hand(int handID) {
+  return this->handList->get(handID);
+}
+
+HandList* Frame::hands() {
+  if(this->handList == nullptr) {
+    this->handList = std::make_shared<godot::leap::HandList>(this->frame.hands());
+  }
+  return this->handList.get();
+}
+
+godot::leap::InteractionBox* Frame::interactionBox() {
+//  return InteractionBox{this->frame.interactionBox()};
+  if(this->box == nullptr) {
+    this->box = std::make_shared<godot::leap::InteractionBox>(this->frame.interactionBox());
+  }
+  return this->box.get();
+}
+
+// Register Methods
+// ----------------
+
+void Frame::_bind_methods() {
+  ClassDB::bind_method(D_METHOD("currentFramesPerSecond"), &Frame::currentFramesPerSecond);
+  ClassDB::bind_method(D_METHOD("id"), &Frame::id);
+  ClassDB::bind_method(D_METHOD("isValid"), &Frame::isValid);
+//  ClassDB::bind_method(D_METHOD("hand"), &Frame::hand);
+//  ClassDB::bind_method(D_METHOD("hands"), &Frame::hands);
+  ClassDB::bind_method(D_METHOD("interactionBox"), &Frame::interactionBox);
+}
